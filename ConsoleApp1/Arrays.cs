eb092cacd0cba5a39ec1a908814f9c649c8a87c7
nyala <nyala@DESKTOP-JTJO6LM>
2020-05-25 00:14:56 -0400

    Initial

--- ConsoleApp1/Arrays.cs
+++ ConsoleApp1/Arrays.cs
@@ -197,47 +197,47 @@ namespace ConsoleApp1
 
 
 
-//        Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+        //        Given an array nums and a value val, remove all instances of that value in-place and return the new length.
 
-//Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+        //Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-//The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+        //The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
-//Example 1:
+        //Example 1:
 
-//Given nums = [3, 2, 2, 3], val = 3,
+        //Given nums = [3, 2, 2, 3], val = 3,
 
-//Your function should return length = 2, with the first two elements of nums being 2.
+        //Your function should return length = 2, with the first two elements of nums being 2.
 
-//It doesn't matter what you leave beyond the returned length.
-//Example 2:
+        //It doesn't matter what you leave beyond the returned length.
+        //Example 2:
 
-//Given nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2,
+        //Given nums = [0, 1, 2, 2, 3, 0, 4, 2], val = 2,
 
-//Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
+        //Your function should return length = 5, with the first five elements of nums containing 0, 1, 3, 0, and 4.
 
-//Note that the order of those five elements can be arbitrary.
+        //Note that the order of those five elements can be arbitrary.
 
-//It doesn't matter what values are set beyond the returned length.
-//Clarification:
+        //It doesn't matter what values are set beyond the returned length.
+        //Clarification:
 
-//Confused why the returned value is an integer but your answer is an array?
+        //Confused why the returned value is an integer but your answer is an array?
 
-//Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+        //Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
 
-//Internally you can think of this:
+        //Internally you can think of this:
 
-//// nums is passed in by reference. (i.e., without making a copy)
-//int len = removeElement(nums, val);
+        //// nums is passed in by reference. (i.e., without making a copy)
+        //int len = removeElement(nums, val);
 
-//// any modification to nums in your function would be known by the caller.
-//// using the length returned by your function, it prints the first len elements.
-//for (int i = 0; i<len; i++) {
-//    print(nums[i]);
-//    }
+        //// any modification to nums in your function would be known by the caller.
+        //// using the length returned by your function, it prints the first len elements.
+        //for (int i = 0; i<len; i++) {
+        //    print(nums[i]);
+        //    }
 
 
-    public static int RemoveElement(int[] nums, int val)
+        public static int RemoveElement(int[] nums, int val)
         {
 
             int returnVal = nums.Length;
@@ -779,8 +779,31 @@ namespace ConsoleApp1
             return y.ToList();
         }
 
+        //printUniquePairsUsingHashtable(new int[] { 2, 4, 7, 5, 9, 10, -1 }, 9);  
+        public static string printUniquePairsUsingHashtable(int[] array, int sum)
+        {
+            StringBuilder sb = new StringBuilder();
 
-
-
+            HashSet<int> hashSet = new HashSet<int>();
+            foreach (int firstNumInPair in array)
+            {
+                int secondNumInPair = sum - firstNumInPair;
+                if (!hashSet.Contains(secondNumInPair))
+                {
+                    if (!hashSet.Add(firstNumInPair))
+                    {
+                        hashSet.Add(secondNumInPair);
+                    }
+                }
+                else
+                {
+                    if (!hashSet.Contains(firstNumInPair))
+                    {
+                        sb.AppendFormat("{0},{1}<br />", firstNumInPair, secondNumInPair);
+                    }
+                }
+            }
+            return sb.ToString(); 
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,51 +12,9 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
 
-            int[] nums = { -10, -7, -3, -2, -1, 4, 5, 6 };
-			/* Start with the empty list. */
-			LinkedList llist = new LinkedList();
+            Arrays.printUniquePairsUsingHashtable(new int[] { 2, 4, 7, 5, 9, 10, -1 }, 9);
 
-			llist.head = new Node(1);
-			Node second = new Node(2);
-			Node third = new Node(3);
-
-			/* Three nodes have been allocated dynamically. 
-			We have references to these three blocks as head, 
-			second and third 
-
-			llist.head	 second			 third 
-				|			 |				 | 
-				|			 |				 | 
-			+----+------+	 +----+------+	 +----+------+ 
-			| 1 | null |	 | 2 | null |	 | 3 | null | 
-			+----+------+	 +----+------+	 +----+------+ */
-
-			llist.head.next = second; // Link first node with the second node 
-
-			/* Now next of first Node refers to second. So they 
-				both are linked. 
-
-			llist.head	 second			 third 
-				|			 |				 | 
-				|			 |				 | 
-			+----+------+	 +----+------+	 +----+------+ 
-			| 1 | o-------->| 2 | null |	 | 3 | null | 
-			+----+------+	 +----+------+	 +----+------+ */
-
-			second.next = third; // Link second node with the third node 
-
-			/* Now next of the second Node refers to third. So all three 
-				nodes are linked. 
-
-			llist.head	 second			 third 
-				|			 |				 | 
-				|			 |				 | 
-			+----+------+	 +----+------+	 +----+------+ 
-			| 1 | o-------->| 2 | o-------->| 3 | null | 
-			+----+------+	 +----+------+	 +----+------+ */
-
-
-		}
+        }
 	}
 
 // A simple C# program to introduce a linked list 
